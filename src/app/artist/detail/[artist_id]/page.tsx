648daec2520927c32eef9b74b523c1a1ca7250f0
flashsoon <developsoon@gmail.com>
2024-02-26 20:57:45 +0900

    style: artist detail page 스타일 적용

--- src/app/artist/detail/[artist_id]/page.tsx
+++ src/app/artist/detail/[artist_id]/page.tsx
@@ -3,7 +3,7 @@ import ArtistDetailContainer from './ArtistDetailContainer'
 import ArtistDetailContents from './ArtistDetailContents'
 
 export default async function ArtistDetailPage({ params }: { params: { artist_id: string } }) {
-  const artistData = await fetch(process.env.BACKEND_API_URL + `artists/${params.artist_id}`, { cache: 'no-store' })
+  const artistData = await fetch(process.env.BACKEND_API_URL + `artists/${params.artist_id}`)
   const artistInfo = await artistData.json()
   return (
     <ArtistDetailContainer>
